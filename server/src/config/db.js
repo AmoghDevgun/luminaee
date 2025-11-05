87abb12c667e7c768d21ce4cd42560d6f523343e
Amogh Devgun <amoghdevgun@gmail.com>
2025-11-05 20:45:21 +0000

    Remove old files

--- server/src/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-import { config } from './env.js'
-
-export async function connectDB() {
-  mongoose.set('strictQuery', true)
-  await mongoose.connect(config.MONGO_URI)
-}
-
-
